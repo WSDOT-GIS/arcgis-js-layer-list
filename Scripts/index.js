733bfe61efbf22ba00bb094d2c8a793805ad21b2
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2015-03-05 12:38:42 -0800

    Added files to repository

---
+++ Scripts/index.js
@@ -0,0 +1,12 @@
+﻿
+/*global require*/
+require(["esri/map"], function (Map) {
+	var map;
+
+	map = new Map("map", {
+		basemap: "hybrid",
+		center: [-120.80566406246835, 47.41322033015946],
+		zoom: 7,
+		showAttribution: true
+	});
+});
