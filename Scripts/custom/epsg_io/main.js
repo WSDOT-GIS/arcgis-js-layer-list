eccda6f1b7677535ded9d3e990c7f4ac43e2fd99
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2015-03-06 16:56:55 -0800

    Added epsg_io package, updated units

---
+++ Scripts/custom/epsg_io/main.js
@@ -0,0 +1,16 @@
+﻿/*global define*/
+define([], function () {
+
+	function CoordinateSystem(/**{object}*/searchResult) {
+		this.accuracy = searchResult.accuracy;
+		this.proj4 = searchResult.proj4;
+		this.code = searchResult.code;
+		this.name = searchResult.name;
+		this.area = searchResult.area;
+		this.bbox = searchResult.bbox;
+		this.trans = searchResult.trans;
+		this.wkt = searchResult.wkt;
+		this.kind = searchResult.kind;
+		this.default_trans = searchResult.default_trans;
+	}
+});
