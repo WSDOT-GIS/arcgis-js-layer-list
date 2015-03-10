5283a8e7a3f4141c63313b40f1994a52f604e14b
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2015-03-10 08:53:43 -0700

    Refactoring
    Moved BufferUI map hookup code into BufferUIHelper.js (formerly BufferLinkInfoWindow.js)

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,45 +1,11 @@
 ﻿
 /*global require*/
-require([
+require(["esri/arcgis/utils",
 	"esri/config",
-	"esri/arcgis/utils",
-	"esri/graphic",
-	"esri/geometry/jsonUtils",
-	"esri/layers/FeatureLayer",
-	"esri/geometry/geometryEngineAsync",
 	"buffer",
-	"buffer/BufferLinkInfoWindow"
-], function (esriConfig, arcgisUtils, Graphic, geometryJsonUtils, FeatureLayer, geometryEngineAsync, BufferUI, BufferLinkInfoWindow) {
-	var buffer, bufferFeatureLayer, oid = 0;
-
-	bufferFeatureLayer = new FeatureLayer({
-		featureSet: null,
-		layerDefinition: {
-			geometryType: "esriGeometryPolygon",
-			fields: [
-				{
-					name: "oid",
-					type: "esriFieldTypeOID"
-				},
-				{
-					name: "distance",
-					type: "esriFieldTypeDouble"
-				},
-				{
-					name: "unit",
-					type: "esriFieldTypeInteger",
-					alias: "Measurement Unit ID"
-				},
-				{
-					name: "unioned",
-					type: "esriFieldTypeSmallInteger",
-					alias: "Is Unioned"
-				}
-			]
-		}
-	}, {
-		className: "buffer"
-	});
+	"buffer/BufferUIHelper"
+], function (arcgisUtils, esriConfig, BufferUI, BufferUIHelper) {
+	var buffer;
 
 	// Specify CORS enabled servers.
 	["www.wsdot.wa.gov", "wsdot.wa.gov", "gispublic.dfw.wa.gov"].forEach(function (svr) {
@@ -56,67 +22,6 @@ require([
 	// Create a map from a predefined webmap on AGOL.
 	arcgisUtils.createMap("927b5daaa7f4434db4b312364489544d", "map").then(function (response) {
 		var map = response.map;
-		map.addLayer(bufferFeatureLayer);
-
-		BufferLinkInfoWindow.addBufferLink(map.infoWindow, buffer);
-
-		buffer.form.addEventListener('clear-graphics', function () {
-			bufferFeatureLayer.clear();
-		});
-
-		buffer.form.addEventListener("buffer", function (e) {
-			var detail = e.detail;
-
-			// Convert regular objects into esri/Geometry objects.
-			if (Array.isArray(detail.geometry)) {
-				detail.geometry = detail.geometry.map(geometryJsonUtils.fromJson, detail.geometry);
-			} else {
-				detail.geometry = geometryJsonUtils.fromJson(detail.geometry);
-			}
-
-			// The geometry engine requires that the number of geometries and distances be the same.
-			// If multiple distances are provided but only a single geometry, that geometry will be
-			// buffered for each distance.
-			if (Array.isArray(detail.distance) && !Array.isArray(detail.geometry)) {
-				detail.geometry = (function () {
-					var outGeoArray = [];
-					for (var i = 0, l = detail.distance.length; i < l; i += 1) {
-						outGeoArray[i] = detail.geometry;
-					}
-					return outGeoArray;
-				}());
-			} else if (!Array.isArray(detail.distance) && Array.isArray(detail.geometry)) {
-				detail.distance = (function () {
-					var outDistanceArray = [];
-					for (var i = 0; i < detail.geometry.length; i++) {
-						outDistanceArray[i] = detail.distance;
-					}
-					return outDistanceArray;
-				}());
-			}
-
-			geometryEngineAsync.buffer(detail.geometry, detail.distance, detail.unit, detail.unionResults).then(function (bufferResults) {
-				console.log("buffer results", bufferResults);
-				if (bufferResults) {
-					bufferFeatureLayer.suspend();
-					if (!Array.isArray(bufferResults)) {
-						bufferResults = [bufferResults];
-					}
-					bufferResults.forEach(function (geometry) {
-						var graphic = new Graphic(geometry, null, {
-							oid: oid++,
-							distance: detail.distance,
-							unit: detail.unit,
-							unioned: detail.unionResults
-						});
-						bufferFeatureLayer.add(graphic);
-					});
-					bufferFeatureLayer.resume();
-				}
-				buffer.clearGeometryList();
-			}, function (error) {
-				console.error("buffer error", error);
-			});
-		});
+		BufferUIHelper.attachBufferUIToMap(map, buffer);
 	});
 });
