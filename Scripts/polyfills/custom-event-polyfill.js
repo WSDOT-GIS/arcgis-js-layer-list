b58e8cd292a0ed0b0da568ae5727785eccc7a4cb
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2015-03-30 09:42:10 -0700

    Replaced polyfill.io with local file.

---
+++ Scripts/polyfills/custom-event-polyfill.js
@@ -0,0 +1,26 @@
+﻿/*jshint -W079 */
+
+// Polyfill for creating CustomEvents on IE9/10/11
+
+// code pulled from:
+// https://github.com/krambuhl/custom-event-polyfill/blob/master/custom-event-polyfill.js
+// https://github.com/d4tocchini/customevent-polyfill
+// https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent#Polyfill
+
+if (!window.CustomEvent || typeof window.CustomEvent !== 'function') {
+	var CustomEvent = function (event, params) {
+		var evt;
+		params = params || {
+			bubbles: false,
+			cancelable: false,
+			detail: undefined
+		};
+
+		evt = document.createEvent("CustomEvent");
+		evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail);
+		return evt;
+	};
+
+	CustomEvent.prototype = window.Event.prototype;
+	window.CustomEvent = CustomEvent; // expose definition to window
+}
