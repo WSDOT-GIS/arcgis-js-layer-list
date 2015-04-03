5b4963e4c8e28c36249776d2a729b05809e479b8
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2015-04-03 13:10:06 -0700

    Added legend-helper package :package:

---
+++ Scripts/custom/legend-helper/LegendResponse.js
@@ -0,0 +1,81 @@
+﻿/*global define*/
+
+define(function () {
+	/**
+	 * Represents an item in a layer's legend.
+	 */
+	function LegendItem(json) {
+		/**@member {string}*/
+		this.label = json.label;
+		/**@member {string}*/
+		this.url = json.url;
+		/**@member {string}*/
+		this.imageData = json.imageData;
+		/**@member {string}*/
+		this.contentType = json.contentType;
+		/**@member {number}*/
+		this.height = json.height || null;
+		/**@member {number}*/
+		this.width = json.width || null;
+		/**@member {number[]}*/
+		this.values = json.values || null;
+	}
+
+	/**
+	 * Represents a layer of a map service.
+	 */
+	function LegendLayer(json) {
+		/**@member {number} */
+		this.layerId = json.layerId;
+		/**@member {string} */
+		this.layerName = json.layerName;
+		/**@member {string} */
+		this.layerType = json.layerType;
+		/**@member {?number}*/
+		this.minScale = json.minScale;
+		/**@member {?number}*/
+		this.maxScale = json.maxScale;
+		/**@member {LegendItem[]}*/
+		this.legend = json.legend.map(function (o) {
+			return new LegendItem(o);
+		});
+	}
+
+	/**
+	 * The top level of a response for a Legend request.
+	 * @param {Object} json
+	 * @param {LegendLayer[]} json.layers
+	 * @class
+	 */
+	function LegendResponse(json) {
+		/**@member {LegendLayer[]}*/
+		this.layers = json.layers.map(function (o) {
+			return new LegendLayer(o);
+
+		});
+	}
+
+	function jsonReviver(k, v) {
+		if (v && v.hasOwnProperty("layers")) {
+			return new LegendResponse(v);
+		}
+		return v;
+	}
+
+	/**
+	 * Parses legend response text into a LegendResponse object.
+	 * @param {string} jsonString - Response from a map service legend request.
+	 * @returns {LegendResponse}
+	 */
+	function parseJson(jsonString) {
+		return JSON.parse(jsonString, jsonReviver);
+	}
+
+	LegendResponse.parseJson = parseJson;
+
+	LegendResponse.LegendLayer = LegendLayer;
+
+	LegendResponse.LegendItem = LegendItem;
+
+	return LegendResponse;
+});
---
+++ Scripts/custom/legend-helper/main.js
@@ -0,0 +1,42 @@
+﻿/*global define*/
+define(["./LegendResponse"], function (LegendResponse) {
+
+	/**
+	 * Gets legend info for a map service.
+	 * @param {(string|esri/layers/layer)} mapServiceUrl - Either the URL of a map service or a Layer class from the ArcGIS API for JavaScript.
+	 * @param {Object[]} [dynamicLayers] - Dynamic layer definitions.
+	 * @returns {Promise}
+	 */
+	function getLegendInfo(mapServiceUrl, dynamicLayers) {
+		if (dynamicLayers) {
+			throw new Error("dynamicLayers support not yet implemented.");
+		}
+		if (typeof mapServiceUrl !== "string" && mapServiceUrl.url) {
+			mapServiceUrl = mapServiceUrl.url;
+		}
+		var promise = new Promise(function (resolve, reject) {
+			var req = new XMLHttpRequest();
+			req.open("GET", mapServiceUrl.replace("/\/$", "") + "/legend?f=json");
+			req.onloadend = function () {
+				var response;
+				if (this.status === 200) {
+					response = LegendResponse.parseJson(this.responseText);
+					if (response.error) {
+						reject(response);
+					} else {
+						resolve(response);
+					}
+				} else {
+					reject({ error: this.statusText });
+				}
+			};
+			req.send();
+		});
+
+		return promise;
+	}
+
+	return {
+		getLegendInfo: getLegendInfo
+	};
+});
