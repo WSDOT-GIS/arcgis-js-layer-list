123e84232a93cc0a2a88d2d82d2afcc1f85b022e
JeffJacobson <JeffJacobson@users.noreply.github.com>
2015-06-16 09:12:43 -0700

    Now loading webmap via esri/request instead of dojo/text

--- demo/index.js
+++ demo/index.js
@@ -1,30 +1,13 @@
 
 /*global require*/
-require(["esri/arcgis/utils",
+require([
+	"esri/request",
+	"esri/arcgis/utils",
 	"esri/config",
 	"esri/domUtils",
-	"layer-list",
-	"dojo/text!./webmap.json"
-], function (arcgisUtils, esriConfig, domUtils, LayerList, webmap) {
+	"layer-list"
+], function (esriRequest, arcgisUtils, esriConfig, domUtils, LayerList) {
 	"use strict";
-
-	// Specify CORS enabled servers.
-	["www.wsdot.wa.gov", "wsdot.wa.gov", "gispublic.dfw.wa.gov"].forEach(function (svr) {
-		esriConfig.defaults.io.corsEnabledServers.push(svr);
-	});
-	// Since CORS servers are explicitly specified, CORS detection is not necessary.
-	// This prevents the following types of errors from appearing in the console:
-	// XMLHttpRequest cannot load http://gis.rita.dot.gov/ArcGIS/rest/info?f=json. No 'Access-Control-Allow-Origin' header is present on the requested resource. Origin 'http://example.com' is therefore not allowed access. 
-	esriConfig.defaults.io.corsDetection = false;
-
-	webmap = JSON.parse(webmap);
-
-	webmap = {
-		item: {
-			extent: [[-126.3619, 44.2285], [-114.3099, 50.0139]],
-		},
-		itemData: webmap
-	};
 
 	/**
 	 * Gets the layer's position in its collection (either map.graphicsLayersIds or map.layerIds).
@@ -54,45 +37,64 @@ require(["esri/arcgis/utils",
 		return ord;
 	}
 
-	domUtils.hide(document.getElementById("mapProgress"));
+	// Specify CORS enabled servers.
+	["www.wsdot.wa.gov", "wsdot.wa.gov", "gispublic.dfw.wa.gov"].forEach(function (svr) {
+		esriConfig.defaults.io.corsEnabledServers.push(svr);
+	});
+	// Since CORS servers are explicitly specified, CORS detection is not necessary.
+	// This prevents the following types of errors from appearing in the console:
+	// XMLHttpRequest cannot load http://gis.rita.dot.gov/ArcGIS/rest/info?f=json. No 'Access-Control-Allow-Origin' header is present on the requested resource. Origin 'http://example.com' is therefore not allowed access. 
+	esriConfig.defaults.io.corsDetection = false;
 
-	// Create a map from a predefined webmap on AGOL.
-	arcgisUtils.createMap(webmap, "map").then(function (response) {
-		var map = response.map;
-		var opLayers = response.itemInfo.itemData.operationalLayers;
+	esriRequest({ url: "webmap.json" }).then(function (response) {
 
-		var layerList = new LayerList(opLayers, document.getElementById("layerlist"));
+		var webmap = {
+			item: {
+				extent: [[-126.3619, 44.2285], [-114.3099, 50.0139]],
+			},
+			itemData: response
+		};
 
-		// Update layer list items to show if they are not visible due to zoom scale.
-		layerList.setScale(map.getScale());
+		// Create a map from a predefined webmap on AGOL.
+		arcgisUtils.createMap(webmap, "map").then(function (response) {
+			domUtils.hide(document.getElementById("mapProgress"));
 
-		map.on("zoom-end", function () {
+			var map = response.map;
+			var opLayers = response.itemInfo.itemData.operationalLayers;
+
+			var layerList = new LayerList(opLayers, document.getElementById("layerlist"));
+
 			// Update layer list items to show if they are not visible due to zoom scale.
 			layerList.setScale(map.getScale());
+
+			map.on("zoom-end", function () {
+				// Update layer list items to show if they are not visible due to zoom scale.
+				layerList.setScale(map.getScale());
+			});
+
+			map.on("update-start", function () {
+				domUtils.show(document.getElementById("mapProgress"));
+			});
+
+			map.on("update-end", function () {
+				domUtils.hide(document.getElementById("mapProgress"));
+			});
+
+			layerList.root.addEventListener("layer-move", function (e) {
+				var detail = e.detail;
+				var movedLayerId = detail.movedLayerId;
+				var targetLayerId = detail.targetLayerId;
+
+				var movedLayer = map.getLayer(movedLayerId);
+
+				var targetLayerOrd = getLayerOrdinal(map, targetLayerId);
+
+				if (targetLayerOrd !== null) {
+					map.reorderLayer(movedLayer, targetLayerOrd);
+				}
+			});
+
+
 		});
-
-		map.on("update-start", function () {
-			domUtils.show(document.getElementById("mapProgress"));
-		});
-
-		map.on("update-end", function () {
-			domUtils.hide(document.getElementById("mapProgress"));
-		});
-
-		layerList.root.addEventListener("layer-move", function (e) {
-			var detail = e.detail;
-			var movedLayerId = detail.movedLayerId;
-			var targetLayerId = detail.targetLayerId;
-
-			var movedLayer = map.getLayer(movedLayerId);
-
-			var targetLayerOrd = getLayerOrdinal(map, targetLayerId);
-
-			if (targetLayerOrd !== null) {
-				map.reorderLayer(movedLayer, targetLayerOrd);
-			}
-		});
-
-		
 	});
 });
